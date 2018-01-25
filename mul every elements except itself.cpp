718411068b6c51c706918240449a8311ef1c5055
limina12 <34646822+limina12@users.noreply.github.com>
2018-01-25 10:08:38 +0530

    Create mul every elements except itself.cpp

---
+++ mul every elements except itself.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n,i,product=1;
+	cin>>n;
+	int a[n],b[n];
+	for(i=0;i<n;i++)
+	{
+	    cin>>a[i];
+	}
+	for(i=0;i<n;i++)
+	{
+	    product=product*a[i];
+	}
+	for(i=0;i<n;i++)
+	{
+	b[i]=(product/a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+	cout<<b[i]<<" ";
+	}return 0;
+}
